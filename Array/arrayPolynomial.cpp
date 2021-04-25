7ffef525a3571ce3dec9716eb2cf8f3c70691c57
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-25 23:47:27 +0530

    Simple transpose sparse matrix added

--- Array/arrayPolynomial.cpp
+++ Array/arrayPolynomial.cpp
@@ -1,144 +1,144 @@
+////
+//// Created by Prasad on 4/24/2021.
+////
 //
-// Created by Prasad on 4/24/2021.
+//#include<iostream>
+//#include<map>
 //
-
-#include<iostream>
-#include<map>
-
-using namespace std;
-
-typedef struct {
-    int coefficient;
-    int exponent;
-}PolynomialTerm;
-
-struct Polynomial{
-    PolynomialTerm polynomial[20];
-    int totalTerms;
-    Polynomial(){
-        totalTerms = 0;
-    }
-    void ReadPolynomial();
-    void DisplayPolynomial();
-    double EvaluatePolynomial(double value);
-    Polynomial addPolynomial(Polynomial secondPolynomial);
-    Polynomial MultiplyPolynomial(Polynomial secondPolynomial);
-};
-
-void Polynomial::ReadPolynomial(){
-    cout << "Enter the total terms in the polynomial " << endl;
-    cin >> totalTerms;
-    for (int i = 0; i <= totalTerms; i++){
-        cout << "Enter the coefficient of " << i+1 << "term: " <<endl;
-        cin >> polynomial[i].coefficient ;
-        cout << "Enter the exponent of " << i+1 << "term: " <<endl;
-        cin >> polynomial[i].exponent;
-    }
-}
-
-void Polynomial::DisplayPolynomial(){
-    for (int index = 0 ; index <= totalTerms ; index++){
-        cout << polynomial[index].coefficient << "x^" <<polynomial[index].exponent << " + " ;
-        cout << "\b";
-    }
-    cout << endl;
-}
-
-double Polynomial::EvaluatePolynomial(double value){
-    double answer = 0;
-    for (int index=0;index<=totalTerms;index++){
-        answer = answer + polynomial[index].coefficient * pow(value,polynomial[index].exponent);
-    }
-    return answer;
-}
-
-Polynomial Polynomial::addPolynomial(Polynomial secondPolynomial) {
-    int k = 0;
-    int j = 0 ;
-    int i = 0;
-    Polynomial resultant;
-    while (i <= totalTerms && j <= secondPolynomial.totalTerms){
-        if(polynomial[i].exponent==secondPolynomial.polynomial[j].exponent){
-            resultant.polynomial[k].exponent = polynomial[i].exponent;
-            resultant.polynomial[k].coefficient = polynomial[i].coefficient + secondPolynomial.polynomial[j].coefficient;
-            i = i + 1;
-            j = j + 1;
-            k = k + 1;
-        }
-        else if (polynomial[i].exponent > secondPolynomial.polynomial[j].exponent){
-            resultant.polynomial[k].exponent = polynomial[i].exponent;
-            resultant.polynomial[k].coefficient = polynomial[i].coefficient;
-            i = i + 1;
-            k = k + 1;
-        }
-        else if(polynomial[i].exponent < secondPolynomial.polynomial[j].exponent){
-            resultant.polynomial[k].exponent = secondPolynomial.polynomial[j].exponent;
-            resultant.polynomial[k].coefficient = secondPolynomial.polynomial[j].coefficient;
-            j = j + 1;
-            k = k + 1;
-        }
-    }
-    if(i > totalTerms){
-        for(j; j<= secondPolynomial.totalTerms;j++){
-            resultant.polynomial[k].coefficient = secondPolynomial.polynomial[j].coefficient;
-            resultant.polynomial[k].exponent = secondPolynomial.polynomial[j].exponent;
-            k = k + 1;
-        }
-    }
-    if(j > secondPolynomial.totalTerms){
-        for(i; i<= totalTerms; i++){
-            resultant.polynomial[k].coefficient = polynomial[i].coefficient;
-            resultant.polynomial[k].exponent = polynomial[i].exponent;
-            k = k + 1;
-        }
-    }
-    resultant.totalTerms = k-1;
-    return resultant;
-}
-
-Polynomial Polynomial::MultiplyPolynomial(Polynomial secondPolynomial) {
-    Polynomial resultant;
-    int k = 0;
-    map<int, int> exponentStorage;
-    int tempCoefficient;
-    int tempExponent;
-    int index = -1;
-    for(int i = 0; i <= totalTerms; i++){
-        for(int j = 0; j <= totalTerms; j++){
-
-            tempCoefficient = polynomial[i].coefficient * secondPolynomial.polynomial[j].coefficient;
-            tempExponent = polynomial[i].exponent + secondPolynomial.polynomial[j].exponent;
-
-            if(exponentStorage[tempExponent]){
-                cout << "Existing "<<tempExponent<<endl;
-                resultant.polynomial[exponentStorage[tempExponent]].exponent += tempExponent;
-                resultant.polynomial[exponentStorage[tempExponent]].coefficient += tempCoefficient;
-            }
-
-            else{
-                cout << "New "<<index<< endl;
-                index = index + 1;
-                exponentStorage[tempExponent] = index;
-                resultant.polynomial[index].exponent = tempExponent;
-                resultant.polynomial[index].coefficient = tempCoefficient;
-            }
-        }
-    }
-    resultant.totalTerms = index;
-    return resultant;
-}
-
-int main(){
-    Polynomial S1, S2, S3;
-    double answer;
-    S1.ReadPolynomial();
-    S2.ReadPolynomial();
-    S1.DisplayPolynomial();
-    S2.DisplayPolynomial();
-//    answer = S.EvaluatePolynomial(23.11);
-//    cout << "The answer is " << answer << endl;
-//    S3 = S1.addPolynomial(S2);
-    S3 = S1.MultiplyPolynomial(S2);
-    S3.DisplayPolynomial();
-    return 0;
-}
+//using namespace std;
+//
+//typedef struct {
+//    int coefficient;
+//    int exponent;
+//}PolynomialTerm;
+//
+//struct Polynomial{
+//    PolynomialTerm polynomial[20];
+//    int totalTerms;
+//    Polynomial(){
+//        totalTerms = 0;
+//    }
+//    void ReadPolynomial();
+//    void DisplayPolynomial();
+//    double EvaluatePolynomial(double value);
+//    Polynomial addPolynomial(Polynomial secondPolynomial);
+//    Polynomial MultiplyPolynomial(Polynomial secondPolynomial);
+//};
+//
+//void Polynomial::ReadPolynomial(){
+//    cout << "Enter the total terms in the polynomial " << endl;
+//    cin >> totalTerms;
+//    for (int i = 0; i <= totalTerms; i++){
+//        cout << "Enter the coefficient of " << i+1 << "term: " <<endl;
+//        cin >> polynomial[i].coefficient ;
+//        cout << "Enter the exponent of " << i+1 << "term: " <<endl;
+//        cin >> polynomial[i].exponent;
+//    }
+//}
+//
+//void Polynomial::DisplayPolynomial(){
+//    for (int index = 0 ; index <= totalTerms ; index++){
+//        cout << polynomial[index].coefficient << "x^" <<polynomial[index].exponent << " + " ;
+//        cout << "\b";
+//    }
+//    cout << endl;
+//}
+//
+//double Polynomial::EvaluatePolynomial(double value){
+//    double answer = 0;
+//    for (int index=0;index<=totalTerms;index++){
+//        answer = answer + polynomial[index].coefficient * pow(value,polynomial[index].exponent);
+//    }
+//    return answer;
+//}
+//
+//Polynomial Polynomial::addPolynomial(Polynomial secondPolynomial) {
+//    int k = 0;
+//    int j = 0 ;
+//    int i = 0;
+//    Polynomial resultant;
+//    while (i <= totalTerms && j <= secondPolynomial.totalTerms){
+//        if(polynomial[i].exponent==secondPolynomial.polynomial[j].exponent){
+//            resultant.polynomial[k].exponent = polynomial[i].exponent;
+//            resultant.polynomial[k].coefficient = polynomial[i].coefficient + secondPolynomial.polynomial[j].coefficient;
+//            i = i + 1;
+//            j = j + 1;
+//            k = k + 1;
+//        }
+//        else if (polynomial[i].exponent > secondPolynomial.polynomial[j].exponent){
+//            resultant.polynomial[k].exponent = polynomial[i].exponent;
+//            resultant.polynomial[k].coefficient = polynomial[i].coefficient;
+//            i = i + 1;
+//            k = k + 1;
+//        }
+//        else if(polynomial[i].exponent < secondPolynomial.polynomial[j].exponent){
+//            resultant.polynomial[k].exponent = secondPolynomial.polynomial[j].exponent;
+//            resultant.polynomial[k].coefficient = secondPolynomial.polynomial[j].coefficient;
+//            j = j + 1;
+//            k = k + 1;
+//        }
+//    }
+//    if(i > totalTerms){
+//        for(j; j<= secondPolynomial.totalTerms;j++){
+//            resultant.polynomial[k].coefficient = secondPolynomial.polynomial[j].coefficient;
+//            resultant.polynomial[k].exponent = secondPolynomial.polynomial[j].exponent;
+//            k = k + 1;
+//        }
+//    }
+//    if(j > secondPolynomial.totalTerms){
+//        for(i; i<= totalTerms; i++){
+//            resultant.polynomial[k].coefficient = polynomial[i].coefficient;
+//            resultant.polynomial[k].exponent = polynomial[i].exponent;
+//            k = k + 1;
+//        }
+//    }
+//    resultant.totalTerms = k-1;
+//    return resultant;
+//}
+//
+//Polynomial Polynomial::MultiplyPolynomial(Polynomial secondPolynomial) {
+//    Polynomial resultant;
+//    int k = 0;
+//    map<int, int> exponentStorage;
+//    int tempCoefficient;
+//    int tempExponent;
+//    int index = -1;
+//    for(int i = 0; i <= totalTerms; i++){
+//        for(int j = 0; j <= totalTerms; j++){
+//
+//            tempCoefficient = polynomial[i].coefficient * secondPolynomial.polynomial[j].coefficient;
+//            tempExponent = polynomial[i].exponent + secondPolynomial.polynomial[j].exponent;
+//
+//            if(exponentStorage[tempExponent]){
+//                cout << "Existing "<<tempExponent<<endl;
+//                resultant.polynomial[exponentStorage[tempExponent]].exponent += tempExponent;
+//                resultant.polynomial[exponentStorage[tempExponent]].coefficient += tempCoefficient;
+//            }
+//
+//            else{
+//                cout << "New "<<index<< endl;
+//                index = index + 1;
+//                exponentStorage[tempExponent] = index;
+//                resultant.polynomial[index].exponent = tempExponent;
+//                resultant.polynomial[index].coefficient = tempCoefficient;
+//            }
+//        }
+//    }
+//    resultant.totalTerms = index;
+//    return resultant;
+//}
+//
+//int main(){
+//    Polynomial S1, S2, S3;
+//    double answer;
+//    S1.ReadPolynomial();
+//    S2.ReadPolynomial();
+//    S1.DisplayPolynomial();
+//    S2.DisplayPolynomial();
+////    answer = S.EvaluatePolynomial(23.11);
+////    cout << "The answer is " << answer << endl;
+////    S3 = S1.addPolynomial(S2);
+//    S3 = S1.MultiplyPolynomial(S2);
+//    S3.DisplayPolynomial();
+//    return 0;
+//}
---
+++ Array/sparseMatrix.cpp
@@ -0,0 +1,156 @@
+////
+//// Created by Prasad on 4/24/2021.
+////
+//
+//#include<iostream>
+//
+//using namespace std;
+//
+//struct SparseMatrix{
+//    const int Max = 20;
+//    int sparseMatrix[Max][3];
+//    void ReadSparseMatrix();
+//    SparseMatrix SimpleTranspose();
+//    SparseMatrix FastTranspose();
+//    SparseMatrix AddSparseMatrix(SparseMatrix B);
+//    SparseMatrix MultiplySparseMatrix(SparseMatrix B);
+//};
+//
+//SparseMatrix SparseMatrix::AddSparseMatrix(SparseMatrix B) {
+//    SparseMatrix C;
+//    int i, j , k , Row1, Row2, Column1, Column2, Value1, Value2;
+//
+//    Row1 = sparseMatrix[0][0];
+//    Column1 = sparseMatrix[0][1];
+//    Value1 = sparseMatrix[0][2];
+//
+//    Row2 = B.sparseMatrix[0][0];
+//    Column2 = B.sparseMatrix[0][1];
+//    Value2 = sparseMatrix[0][2];
+//
+//    if (Row1==Row2 && Column1==Column2){
+//
+//        i = j = k = 1;
+//
+//        C.sparseMatrix[0][0] = sparseMatrix[0][0];
+//        C.sparseMatrix[0][1] = sparseMatrix[0][1];
+//
+//        while (i <= Value1 && j <= Value2) {
+//            if (sparseMatrix[i][0] == B.sparseMatrix[j][0]){
+//                if (sparseMatrix[i][1] == B.sparseMatrix[j][1]){
+//                    C.sparseMatrix[k][0] = sparseMatrix[i][0];
+//                    C.sparseMatrix[k][1] = sparseMatrix[i][1];
+//                    C.sparseMatrix[k][2] = sparseMatrix[i][2];
+//                    i++;
+//                    j++;
+//                    k++;
+//                }
+//                else {
+//                    if (sparseMatrix[i][1] < B.sparseMatrix[i][1]){
+//                        C.sparseMatrix[k][0] = sparseMatrix[i][0];
+//                        C.sparseMatrix[k][1] = sparseMatrix[i][1];
+//                        C.sparseMatrix[k][2] = sparseMatrix[i][2];
+//                        i++;
+//                        k++;
+//                    }
+//                    else {
+//                        C.sparseMatrix[k][0] = B.sparseMatrix[j][0];
+//                        C.sparseMatrix[k][1] = B.sparseMatrix[j][1];
+//                        C.sparseMatrix[k][2] = B.sparseMatrix[j][2];
+//                        j++;
+//                        k++;
+//                    }
+//                }
+//            }
+//            else if{sparseMatrix[i][0] < B.sparseMatrix[j][0]}{
+//                C.sparseMatrix[k][0] = sparseMatrix[i][0];
+//                C.sparseMatrix[k][1] = sparseMatrix[i][1];
+//                C.sparseMatrix[k][2] = sparseMatrix[i][2];
+//                i++;
+//                k++;
+//            }
+//            else{
+//                C.sparseMatrix[k][0] = B.sparseMatrix[j][0];
+//                C.sparseMatrix[k][1] = B.sparseMatrix[j][1];
+//                C.sparseMatrix[k][2] = B.sparseMatrix[j][2];
+//                j++;
+//                k++;
+//            }
+//        }
+//
+//        while(i <= Value1){
+//            C.sparseMatrix[k][0] = sparseMatrix[i][0];
+//            C.sparseMatrix[k][0] = sparseMatrix[i][1];
+//            C.sparseMatrix[k][0] = sparseMatrix[i][2];
+//            i++;
+//            k++;
+//        }
+//
+//        while(j <= Value2){
+//            C.sparseMatrix[k][0] = sparseMatrix[j][0];
+//            C.sparseMatrix[k][1] = sparseMatrix[j][1];
+//            C.sparseMatrix[k][2] = sparseMatrix[j][2];
+//            j++;
+//            k++;
+//        }
+//    }
+//    else{
+//        cout << "Matrix cannot be added as they are not of the same dimension" << endl;
+//    }
+//
+//}
+//
+//SparseMatrix SparseMatrix::SimpleTranspose(){
+//    int Row = sparseMatrix[0][0], Column = sparseMatrix[0][1], Value = sparseMatrix[0][2];
+//    SparseMatrix transposeMatrix;
+//    transposeMatrix.sparseMatrix[0][0] = Column;
+//    transposeMatrix.sparseMatrix[0][1] = Row;
+//    transposeMatrix.sparseMatrix[0][2] = Value;
+//
+//    if(Value==0){
+//        return transposeMatrix;
+//    }
+//
+//    for(int i = 0; i < Column; i++){
+//        for(int j = 1; j <= Value; j++){
+//            if(sparseMatrix[j][1] == j){
+//                transposeMatrix.sparseMatrix[i][0] = sparseMatrix[i][1];
+//                transposeMatrix.sparseMatrix[i][1] = sparseMatrix[i][0];
+//                transposeMatrix.sparseMatrix[i][2] = sparseMatrix[i][2];
+//                i++;
+//            }
+//        }
+//    }
+//    return transposeMatrix;
+//}
+//
+////SparseMatrix SparseMatrix::FastTranspose() {
+////    int Row, Col, Value, Frequency[], RowStartPosition[];
+////    SparseMatrix transposeMatrix;
+////
+////    Row = sparseMatrix[0][0];
+////    Col = sparseMatrix[0][1];
+////    Value = sparseMatrix[0][2];
+////
+////    transposeMatrix.sparseMatrix[0][0] = Col;
+////    transposeMatrix.sparseMatrix[0][1] = Row;
+////    transposeMatrix.sparseMatrix[0][2] = Value;
+////
+////    if (Value==0){
+////        return transposeMatrix;
+////    }
+////
+////    for(int i=0; i < Col ; i++){
+////        Frequency[i] = 0;
+////    }
+////
+////    for(int j=1; j <=Value; j++){
+////        Frequency[sparseMatrix[j][1]] += 1;
+////    }
+////
+////    RowStartPosition[0] = 1;
+////
+////    for(int k=1; k<Col ; k++){
+////
+////    }
+////}
