e597899812acdd8c1df713c1fb03cf94d05b9eb4
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-29 12:33:12 +0530

    Code refactored

--- Stack/ExpressionConversion/postfix_prefix.cpp
+++ Stack/ExpressionConversion/postfix_prefix.cpp
@@ -1,123 +1,123 @@
+////
+//// Created by Prasad on 4/27/2021.
+////
 //
-// Created by Prasad on 4/27/2021.
+//#include<iostream>
+//#include<cstring>
 //
-
-#include<iostream>
-#include<cstring>
-
-using namespace std;
-
-struct Stack{
-    char *StackArr[5];
-    int MaxSize;
-    int Top;
-    int currentSize;
-    Stack(){
-        MaxSize = 20;
-        Top = -1;
-        currentSize = 0;
-    }
-
-    void push(char * elementString) {
-        try{
-            if(!isFull()){
-                cout << "Prepush " << elementString << endl;
-                StackArr[++Top] = elementString;
-                cout << "Post push " << StackArr[Top] << endl;
-            }
-            else throw overflow_error("");
-        }
-        catch(overflow_error &e){
-            cout << e.what();
-        }
-    }
-
-
-    char * pop() {
-        try {
-            if (!isEmpty()){
-                cout << "Pre pop " << StackArr[Top] << endl;
-                return StackArr[Top--];
-            }
-            else throw underflow_error("");
-        }
-        catch (underflow_error &e) {
-            cout << e.what() ;
-        }
-    }
-
-    bool isFull() {
-        if (Top == (MaxSize - 1)){
-            return true;
-        }
-        return false;
-    }
-
-    bool isEmpty(){
-        if(Top == -1){
-            return true;
-        }
-        return false;
-    }
-
-    int CurrentSize(){
-        return Top + 1;
-    }
-
-};
-
-struct PostfixPrefix {
-
-    Stack Resultant, operands;
-    char Expression[50];
-
-    PostfixPrefix(){
-        cin.getline(Expression, sizeof(Expression));
-    }
-
-    const char * convertToPrefix(){
-        int i = 0;
-        char *tempChar;
-        char *operand_1;
-        char *operand_2;
-        char *tempString;
-        char tempCharString[2];
-        while(Expression[i] != '='){
-            tempChar = &Expression[i];
-            tempCharString[0] = Expression[i];
-            tempCharString[1] = '\0';
-            if(*tempChar == '+' || *tempChar == '-' || *tempChar == '*' || *tempChar == '/' || *tempChar == '^'){
-                tempString = new char[20];
-                operand_2 = operands.pop();
-                operand_1 = operands.pop();
-                strcpy(tempString,tempCharString);
-                cout <<"ts string 1"<< tempString << endl;
-                strcat(tempString,operand_1);
-                cout <<"ts string 2"<< tempString << endl;
-                strcat(tempString,operand_2);
-                cout <<"ts string 3"<< tempString << endl;
-                operands.push(tempString);
-            }
-
-            else{
-                tempString = new char[2];
-                *tempString = *tempChar;
-                *(tempString + 1) = '\0';
-                operands.push(tempString);
-            }
-
-            i++;
-        }
-
-        return operands.StackArr[0];
-    }
-};
-
-int main() {
-    cout << "Enter the expression (postfix) and add '=' at the end: " << endl;
-    PostfixPrefix S;
-    cout << S.convertToPrefix();
-    return 0;
-}
-
-//g++ postfix_prefix.cpp -o postfix_prefix.exe && postfix_prefix.exe
+//using namespace std;
+//
+//struct Stack{
+//    char *StackArr[5];
+//    int MaxSize;
+//    int Top;
+//    int currentSize;
+//    Stack(){
+//        MaxSize = 20;
+//        Top = -1;
+//        currentSize = 0;
+//    }
+//
+//    void push(char * elementString) {
+//        try{
+//            if(!isFull()){
+//                cout << "Prepush " << elementString << endl;
+//                StackArr[++Top] = elementString;
+//                cout << "Post push " << StackArr[Top] << endl;
+//            }
+//            else throw overflow_error("");
+//        }
+//        catch(overflow_error &e){
+//            cout << e.what();
+//        }
+//    }
+//
+//
+//    char * pop() {
+//        try {
+//            if (!isEmpty()){
+//                cout << "Pre pop " << StackArr[Top] << endl;
+//                return StackArr[Top--];
+//            }
+//            else throw underflow_error("");
+//        }
+//        catch (underflow_error &e) {
+//            cout << e.what() ;
+//        }
+//    }
+//
+//    bool isFull() {
+//        if (Top == (MaxSize - 1)){
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    bool isEmpty(){
+//        if(Top == -1){
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    int CurrentSize(){
+//        return Top + 1;
+//    }
+//
+//};
+//
+//struct PostfixPrefix {
+//
+//    Stack Resultant, operands;
+//    char Expression[50];
+//
+//    PostfixPrefix(){
+//        cin.getline(Expression, sizeof(Expression));
+//    }
+//
+//    const char * convertToPrefix(){
+//        int i = 0;
+//        char *tempChar;
+//        char *operand_1;
+//        char *operand_2;
+//        char *tempString;
+//        char tempCharString[2];
+//        while(Expression[i] != '='){
+//            tempChar = &Expression[i];
+//            tempCharString[0] = Expression[i];
+//            tempCharString[1] = '\0';
+//            if(*tempChar == '+' || *tempChar == '-' || *tempChar == '*' || *tempChar == '/' || *tempChar == '^'){
+//                tempString = new char[20];
+//                operand_2 = operands.pop();
+//                operand_1 = operands.pop();
+//                strcpy(tempString,tempCharString);
+//                cout <<"ts string 1"<< tempString << endl;
+//                strcat(tempString,operand_1);
+//                cout <<"ts string 2"<< tempString << endl;
+//                strcat(tempString,operand_2);
+//                cout <<"ts string 3"<< tempString << endl;
+//                operands.push(tempString);
+//            }
+//
+//            else{
+//                tempString = new char[2];
+//                *tempString = *tempChar;
+//                *(tempString + 1) = '\0';
+//                operands.push(tempString);
+//            }
+//
+//            i++;
+//        }
+//
+//        return operands.StackArr[0];
+//    }
+//};
+//
+//int main() {
+//    cout << "Enter the expression (postfix) and add '=' at the end: " << endl;
+//    PostfixPrefix S;
+//    cout << S.convertToPrefix();
+//    return 0;
+//}
+//
+////g++ postfix_prefix.cpp -o postfix_prefix.exe && postfix_prefix.exe
--- Stack/ExpressionConversion/prefix_postfix.cpp
+++ Stack/ExpressionConversion/prefix_postfix.cpp
@@ -1,137 +1,128 @@
+
+// Created by Prasad on 4/27/2021.
 //
-//// Created by Prasad on 4/27/2021.
-////
-//
-//#include<iostream>
-//
-//using namespace std;
-//
-//struct Stack{
-//    char StackArr[20];
-//    int MaxSize;
-//    int Top;
-//    int currentSize;
-//    Stack(){
-//        MaxSize = 20;
-//        Top = -1;
-//        currentSize = 0;
-//    }
-//
-//    void push(char element) {
-//        try{
-//            if(!isFull()){
-//                StackArr[++Top] = element;
-//            }
-//            else throw overflow_error("");
-//        }
-//        catch(overflow_error &e){
-//            cout << e.what();
-//        }
-//    }
-//
-//
-//    char getTop() {
-//        try {
-//            if(!isEmpty()){
-//                return StackArr[Top];
-//            }
-//            else{
-////                throw underflow_error("");
-//                return 'p';
-//            }
-//        }
-//        catch (underflow_error e) {
-//            cout << e.what();
-//        }
-//    }
-//
-//    char pop() {
-//        try {
-//            if (!isEmpty()){
-//                return StackArr[Top--];
-//            }
-//            else throw underflow_error("");
-//        }
-//        catch (underflow_error &e) {
-//            cout << e.what() ;
-//        }
-//    }
-//
-//    bool isFull() {
-//        if (Top == (MaxSize - 1)){
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    bool isEmpty(){
-//        if(Top == -1){
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    int CurrentSize(){
-//        return Top + 1;
-//    }
-//
-//};
-//
-//struct PrefixPostfix {
-//
-//    Stack Resultant, operands;
-//    char Expression[50];
-//
-//    PrefixPostfix(){
-//        cin.getline(Expression, sizeof(Expression));
-//    }
-//
-//    int length() {
-//        int i = 0;
-//        while (Expression[i] != '='){
-//            i++;
-//        }
-//        return i-1;
-//    }
-//
-//    const char * convertToPostfix(){
-//        int i = length();
-//        char tempChar;
-//        char operand_1;
-//        char operand_2;
-//        while(i>=0){
-//            tempChar = Expression[i];
-//            if(tempChar == '+' || tempChar == '-' || tempChar == '*' || tempChar == '/' || tempChar == '^'){
-//                operand_2 = operands.pop();
-//                operand_1 = operands.pop();
-//                Resultant.push(tempChar);
-//                Resultant.push(operand_1);
-//                Resultant.push(operand_2);
-//            }
-//
-//            else{
-//                operands.push(Expression[i]);
-//            }
-//
-//            i--;
-//        }
-//
-//        Resultant.push('\0');
-//        char temp;
-//        for(int i = 0; i < (Resultant.Top-1)/2; i++){
-//            temp = Resultant.StackArr[i];
-//            Resultant.StackArr[i] = Resultant.StackArr[Resultant.Top - i - 1];
-//            Resultant.StackArr[Resultant.Top - i - 1] = temp;
-//        }
-//        return Resultant.StackArr;
-//    }
-//};
-//
-//int main() {
-//    cout << "Enter the expression (postfix) and add '=' at the end: " << endl;
-//    PrefixPostfix S;
-//    cout << S.convertToPostfix();
-//    return 0;
-//}
-//
-////g++ prefix_postfix.cpp -o prefix_postfix.exe && prefix_postfix.exe
+
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+struct Stack{
+    char *StackArr[5];
+    int MaxSize;
+    int Top;
+    int currentSize;
+    Stack(){
+        MaxSize = 20;
+        Top = -1;
+        currentSize = 0;
+    }
+
+    void push(char * elementString) {
+        try{
+            if(!isFull()){
+                cout << "Prepush " << elementString << endl;
+                StackArr[++Top] = elementString;
+                cout << "Post push " << StackArr[Top] << endl;
+            }
+            else throw overflow_error("");
+        }
+        catch(overflow_error &e){
+            cout << e.what();
+        }
+    }
+
+
+    char * pop() {
+        try {
+            if (!isEmpty()){
+                cout << "Pre pop " << StackArr[Top] << endl;
+                return StackArr[Top--];
+            }
+            else throw underflow_error("");
+        }
+        catch (underflow_error &e) {
+            cout << e.what() ;
+        }
+    }
+
+    bool isFull() {
+        if (Top == (MaxSize - 1)){
+            return true;
+        }
+        return false;
+    }
+
+    bool isEmpty(){
+        if(Top == -1){
+            return true;
+        }
+        return false;
+    }
+
+    int CurrentSize(){
+        return Top + 1;
+    }
+
+};
+
+struct PrefixPostfix {
+
+    Stack Resultant, operands;
+    char Expression[50];
+
+    PrefixPostfix(){
+        cin.getline(Expression, sizeof(Expression));
+    }
+
+    int length() {
+        int i = 0;
+        while (Expression[i] != '='){
+            i++;
+        }
+        return i-1;
+    }
+
+    const char * convertToPostfix(){
+        int i = length();
+        char *tempChar;
+        char *operand_1;
+        char *operand_2;
+        char *tempString;
+        char tempCharString[2];
+        while(i>=0){
+            tempChar = &Expression[i];
+            tempCharString[0] = Expression[i];
+            tempCharString[1] = '\0';
+            if(*tempChar == '+' || *tempChar == '-' || *tempChar == '*' || *tempChar == '/' || *tempChar == '^'){
+                tempString = new char[20];
+                operand_1 = operands.pop();
+                operand_2 = operands.pop();
+                strcpy(tempString, operand_1);
+                strcat(tempString, operand_2);
+                strcat(tempString, tempCharString);
+                operands.push(tempString);
+            }
+
+            else{
+                tempString = new char[2];
+                *tempString = *tempChar;
+                *(tempString + 1) = '\0';
+                operands.push(tempString);
+            }
+
+            i--;
+        }
+
+        return operands.StackArr[0];
+    }
+};
+
+int main() {
+    cout << "Enter the expression (prefix) and add '=' at the end: " << endl;
+    PrefixPostfix S;
+    cout << S.convertToPostfix();
+    return 0;
+}
+
+//g++ prefix_postfix.cpp -o prefix_postfix.exe && prefix_postfix.exe
