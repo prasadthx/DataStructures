64721499bec5d842c837fd9d5f1277f18742647e
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-05-09 12:35:51 +0530

    Sentinel Search added

--- Searching/SequentialSearch.cpp
+++ Searching/SequentialSearch.cpp
@@ -1,46 +1,46 @@
+////
+//// Created by Prasad on 5/9/2021.
+////
 //
-// Created by Prasad on 5/9/2021.
+//#include<iostream>
 //
-
-#include<iostream>
-
-using namespace std;
-
-int sequentialSearch(int arrayLength, int *array,int element) {
-    for (int i = 0; i < arrayLength; i++){
-        if (*(array + i) == element){
-            return i + 1;
-        }
-    }
-    return -1;
-}
-
-int main(){
-
-    int arrayLength, element, *array, resultant;
-
-    cout << "Sequential Search implementation: " << endl;
-    cout << "Enter the number of elements: " << endl;
-
-    cin >> arrayLength;
-    array = new int[arrayLength];
-
-    for (int i = 0; i < arrayLength; i++){
-        cout << "Enter the element in array: " << endl;
-        cin >> *(array + i);
-    }
-
-    cout << "Enter the element to be searched: " << endl;
-    cin >> element;
-
-
-    resultant = sequentialSearch(arrayLength, array, element);
-
-    if(resultant == -1){
-        cout << "The element was not found.";
-        return 0;
-    }
-
-    cout << "The element was found at position: " << resultant << endl;
-    return 0;
-}
+//using namespace std;
+//
+//int sequentialSearch(int arrayLength, int *array,int element) {
+//    for (int i = 0; i < arrayLength; i++){
+//        if (*(array + i) == element){
+//            return i + 1;
+//        }
+//    }
+//    return -1;
+//}
+//
+//int main(){
+//
+//    int arrayLength, element, *array, resultant;
+//
+//    cout << "Sequential Search implementation: " << endl;
+//    cout << "Enter the number of elements: " << endl;
+//
+//    cin >> arrayLength;
+//    array = new int[arrayLength];
+//
+//    for (int i = 0; i < arrayLength; i++){
+//        cout << "Enter the element in array: " << endl;
+//        cin >> *(array + i);
+//    }
+//
+//    cout << "Enter the element to be searched: " << endl;
+//    cin >> element;
+//
+//
+//    resultant = sequentialSearch(arrayLength, array, element);
+//
+//    if(resultant == -1){
+//        cout << "The element was not found.";
+//        return 0;
+//    }
+//
+//    cout << "The element was found at position: " << resultant << endl;
+//    return 0;
+//}
