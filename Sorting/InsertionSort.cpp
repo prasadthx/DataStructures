53222659a34c181436e68ae19d5c1ca09723b1cb
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-05-10 17:40:55 +0530

    Selection Sort Added

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,47 +1,50 @@
+////
+//// Created by Prasad on 5/10/2021.
+////
 //
-// Created by Prasad on 5/10/2021.
+//#include<iostream>
 //
+//using namespace std;
+//
+//void InsertionSort(int arrayLength, int *array){
+//    int j,temp, compareElement;
+//    for(int i = 1; i < arrayLength; i++){
+//        compareElement = *(array + i);
+//        j = i;
+//        while(j > 0 && *(array + j - 1) > compareElement){
+//            *(array + j) = *(array + j - 1);
+//            j--;
+//        }
+//        *(array + j) = compareElement;
+//    }
+//}
+//
+//int main(){
+//    int *array, arrayLength;
+//    cout << "Insertion Sort Implementation" << endl;
+//
+//    cout << "Enter the number of elements: " << endl;
+//    cin >> arrayLength;
+//
+//    array = new int[arrayLength];
+//
+//    for(int i = 0; i < arrayLength; i++){
+//        cout << "Enter the unsorted list elements: ";
+//        cin >> *(array + i);
+//    }
+//
+//    cout << "\nUnsorted array: " ;
+//    for(int i = 0; i < arrayLength; i++){
+//        cout << *(array + i) << "  " ;
+//    }
+//
+//    InsertionSort(arrayLength, array);
+//
+//    cout << "\nSorted array: " ;
+//    for(int i = 0; i < arrayLength; i++){
+//        cout << *(array + i) << "  " ;
+//    }
+//}
 
-#include<iostream>
 
-using namespace std;
-
-void InsertionSort(int arrayLength, int *array){
-    int j,temp, compareElement;
-    for(int i = 1; i < arrayLength; i++){
-        compareElement = *(array + i);
-        j = i;
-        while(j > 0 && *(array + j - 1) > compareElement){
-            *(array + j) = *(array + j - 1);
-            j--;
-        }
-        *(array + j) = compareElement;
-    }
-}
-
-int main(){
-    int *array, arrayLength;
-    cout << "Insertion Sort Implementation" << endl;
-
-    cout << "Enter the number of elements: " << endl;
-    cin >> arrayLength;
-
-    array = new int[arrayLength];
-
-    for(int i = 0; i < arrayLength; i++){
-        cout << "Enter the unsorted list elements: ";
-        cin >> *(array + i);
-    }
-
-    cout << "\nUnsorted array: " ;
-    for(int i = 0; i < arrayLength; i++){
-        cout << *(array + i) << "  " ;
-    }
-
-    InsertionSort(arrayLength, array);
-
-    cout << "\nSorted array: " ;
-    for(int i = 0; i < arrayLength; i++){
-        cout << *(array + i) << "  " ;
-    }
-}
+////clang++ InsertionSort.cpp -o InsertionSort.exe && InsertionSort.exe
