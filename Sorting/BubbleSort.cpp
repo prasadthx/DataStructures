3e6267d250749506cd6f38337ddc86270e332937
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-05-10 16:55:37 +0530

    Insertion Sort added

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,47 +1,47 @@
+////
+//// Created by Prasad on 5/10/2021.
+////
 //
-// Created by Prasad on 5/10/2021.
+//#include<iostream>
 //
-
-#include<iostream>
-
-using namespace std;
-
-void BubbleSort(int arrayLength, int *array){
-    int temp;
-    for(int i = 0; i < arrayLength; i++){
-        for(int j = 1; j < (arrayLength - i) ; j++){
-            if(*(array + j - 1) > *(array + j)){
-                temp = *(array + j - 1);
-                *(array + j - 1) = *(array + j);
-                *(array + j) = temp;
-            }
-        }
-    }
-}
-
-int main(){
-    int *array, arrayLength;
-    cout << "Bubble Sort Implementation" << endl;
-
-    cout << "Enter the number of elements: " << endl;
-    cin >> arrayLength;
-
-    array = new int[arrayLength];
-
-    for(int i = 0; i < arrayLength; i++){
-        cout << "Enter the unsorted list elements: ";
-        cin >> *(array + i);
-    }
-
-    cout << "\nUnsorted array: " ;
-    for(int i = 0; i < arrayLength; i++){
-        cout << *(array + i) << "  " ;
-    }
-
-    BubbleSort(arrayLength, array);
-
-    cout << "\nSorted array: " ;
-    for(int i = 0; i < arrayLength; i++){
-        cout << *(array + i) << "  " ;
-    }
-}
+//using namespace std;
+//
+//void BubbleSort(int arrayLength, int *array){
+//    int temp;
+//    for(int i = 0; i < arrayLength; i++){
+//        for(int j = 1; j < (arrayLength - i) ; j++){
+//            if(*(array + j - 1) > *(array + j)){
+//                temp = *(array + j - 1);
+//                *(array + j - 1) = *(array + j);
+//                *(array + j) = temp;
+//            }
+//        }
+//    }
+//}
+//
+//int main(){
+//    int *array, arrayLength;
+//    cout << "Bubble Sort Implementation" << endl;
+//
+//    cout << "Enter the number of elements: " << endl;
+//    cin >> arrayLength;
+//
+//    array = new int[arrayLength];
+//
+//    for(int i = 0; i < arrayLength; i++){
+//        cout << "Enter the unsorted list elements: ";
+//        cin >> *(array + i);
+//    }
+//
+//    cout << "\nUnsorted array: " ;
+//    for(int i = 0; i < arrayLength; i++){
+//        cout << *(array + i) << "  " ;
+//    }
+//
+//    BubbleSort(arrayLength, array);
+//
+//    cout << "\nSorted array: " ;
+//    for(int i = 0; i < arrayLength; i++){
+//        cout << *(array + i) << "  " ;
+//    }
+//}
---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Prasad on 5/10/2021.
+//
+
+#include<iostream>
+
+using namespace std;
+
+void InsertionSort(int arrayLength, int *array){
+    int j,temp, compareElement;
+    for(int i = 1; i < arrayLength; i++){
+        compareElement = *(array + i);
+        j = i;
+        while(j > 0 && *(array + j - 1) > compareElement){
+            *(array + j) = *(array + j - 1);
+            j--;
+        }
+        *(array + j) = compareElement;
+    }
+}
+
+int main(){
+    int *array, arrayLength;
+    cout << "Insertion Sort Implementation" << endl;
+
+    cout << "Enter the number of elements: " << endl;
+    cin >> arrayLength;
+
+    array = new int[arrayLength];
+
+    for(int i = 0; i < arrayLength; i++){
+        cout << "Enter the unsorted list elements: ";
+        cin >> *(array + i);
+    }
+
+    cout << "\nUnsorted array: " ;
+    for(int i = 0; i < arrayLength; i++){
+        cout << *(array + i) << "  " ;
+    }
+
+    InsertionSort(arrayLength, array);
+
+    cout << "\nSorted array: " ;
+    for(int i = 0; i < arrayLength; i++){
+        cout << *(array + i) << "  " ;
+    }
+}
