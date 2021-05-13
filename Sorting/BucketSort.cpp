22d38b207167f5ec3ee3059be61bf93256505f31
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-05-13 19:43:42 +0530

    Bucket sort added

--- Sorting/BucketSort.cpp
+++ Sorting/BucketSort.cpp
@@ -2,3 +2,57 @@
 // Created by Prasad on 5/13/2021.
 //
 
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+void BucketSort(float array[], int arrayLength){
+    vector<float> bucketArray [arrayLength];
+
+    for (int i=0; i < arrayLength; i++){
+        bucketArray[int(arrayLength*array[i])].push_back(array[i]);
+    }
+
+    for (int i=0; i < arrayLength; i++){
+        sort(bucketArray[i].begin(), bucketArray[i].end());
+    }
+
+    int index = 0;
+    for (int i=0; i < arrayLength; i++){
+        for(int j=0; j < bucketArray[i].size(); j++){
+            array[index++] = bucketArray[i][j];
+        }
+    }
+}
+
+int main(){
+    float *array, arrayLength;
+    cout << "Bucket Sort Implementation" << endl;
+
+    cout << "Enter the number of elements: " << endl;
+    cin >> arrayLength;
+
+    array = new float[arrayLength];
+
+    for(int i = 0; i < arrayLength; i++){
+        cout << "Enter the unsorted list elements: ";
+        cin >> *(array + i);
+    }
+
+    cout << "\nUnsorted array: " ;
+    for(int i = 0; i < arrayLength; i++){
+        cout << *(array + i) << "  " ;
+    }
+
+    BucketSort(array, arrayLength);
+
+    cout << "\nSorted array: " ;
+    for(int i = 0; i < arrayLength; i++){
+        cout << *(array + i) << "  " ;
+    }
+}
+
+
+//clang++ BucketSort.cpp -o BucketSort.exe && BucketSort.exe
