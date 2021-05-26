a48fc42fce9137510665d957f665ac52b7caeaac
prasad <prasadzore31@gmail.com>
2021-05-26 16:54:11 +0530

    Binary Tree added

--- Trees/BInaryTreeADT.cpp
+++ Trees/BInaryTreeADT.cpp
@@ -53,8 +53,6 @@ public:
         }
     };
 
-//    void DeleteNode(Node *);
-
     void InOrder(Node *node){
         if(node == nullptr){
             return;
@@ -63,24 +61,87 @@ public:
         cout << node->data << " " << endl;
         InOrder(node->rightChild);
     };
-//
-//    void PreOrder(Node *);
-//
-//    void PostOrder(Node *);
-//
-//    Node * CopyTree();
-//
-//    void MirrorTree();
-//
-//    void TreeHeight(Node *);
-//
-//    int CountLeaf(Node *);
-//
-//    int CountNode(Node *);
-//
-//    void BreadthFirstTree(Node *);
-//
-//    void DepthFirstTree(Node *);
 
+    void PreOrder(Node *node){
+        if(node == nullptr){
+            return;
+        }
+        cout << node->data << " " << endl;
+        InOrder(node->leftChild);
+        InOrder(node->rightChild);
+    };
+
+    void PostOrder(Node *node){
+        if(node == nullptr){
+            return;
+        }
+        InOrder(node->leftChild);
+        InOrder(node->rightChild);
+        cout << node->data << " " << endl;
+    };
+
+    Node * CopyTree(){
+
+        Node *temp;
+
+        if(rootNode == nullptr){
+            return nullptr;
+        }
+
+        temp = new Node(rootNode->data);
+
+        temp->rightChild = CopyTree(temp->rightChild);
+        temp->leftChild = CopyTree(temp->leftChild);
+
+        return temp;
+    };
+
+    void MirrorTree(Node *node){
+        if (node==nullptr || (node->rightChild==nullptr && node->leftChild== nullptr)){
+            return;
+        }
+        Node *temp = node->leftChild;
+        node->leftChild = node->rightChild;
+        node->rightChild = temp;
+
+        MirrorTree(node->left);
+        MirrorTree(node->right);
+    };
+
+    int TreeHeight(Node *node){
+        if(node == nullptr){
+            return 0;
+        }
+        if(node->rightChild == nullptr && node->leftChild == nullptr){
+            return 1;
+        }
+
+        int leftHeight = 1 + TreeHeight(node->leftChild);
+        int rightHeight = 1 + TreeHeight(node->rightChild);
+
+        if (leftHeight>rightHeight){
+            return leftHeight;
+        }
+        else
+            return rightHeight;
+
+    };
+
+    int CountLeaf(Node *node){
+        if (node == nullptr){
+            return 0;
+        }
+        if(node->leftChild == nullptr && node->rightChild == nullptr){
+            return 1;
+        }
+        return (CountLeaf(node->leftChild) + CountLeaf(node->rightChild));
+    };
+
+    int CountNode(Node *node){
+        if (node == nullptr){
+            return 0;
+        }
+        return (1 + CountNode(node->rightChild) + CountNode(node->leftChild));
+    };
 
 };
