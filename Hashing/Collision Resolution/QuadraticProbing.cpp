d2340e5aa1c6bd865761aad45c0ddba811268dca
prasad <prasadzore31@gmail.com>
2021-05-26 15:17:14 +0530

    Binary Tree added

---
+++ Hashing/Collision Resolution/QuadraticProbing.cpp
@@ -0,0 +1,58 @@
+//
+// Created by prasad on 5/25/21.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class HashTable{
+
+    int MAX_SIZE;
+    int *hashTable;
+
+public:
+
+    HashTable(int MAX_SIZE){
+        this->MAX_SIZE = MAX_SIZE;
+        this->hashTable = new int[MAX_SIZE];
+        for(int i = 0; i < MAX_SIZE; i++){
+            hashTable[i] = -1;
+        }
+    }
+
+    void displayHashTable(){
+        for (int i = 0; i < MAX_SIZE; i++){
+            cout << "Index: " << i << " => " << hashTable[i] << endl;
+        }
+    }
+
+    void InsertValue(int key){
+        int index = key % MAX_SIZE;
+        if (hashTable[index] == -1){
+            hashTable[index] = key;
+        }
+        else{
+            int probeIndex = 1;
+            while (hashTable[index] != -1){
+                index = (key + probeIndex*probeIndex) % MAX_SIZE;
+                index++ ;
+            }
+            hashTable[index] = key;
+        }
+    }
+
+};
+
+int main(){
+    HashTable h1(20);
+    for (int i = 5; i < 25; i+=2){
+        h1.InsertValue(i);
+    }
+    h1.displayHashTable();
+}
+
+//clang++ QuadraticProbing.cpp -o QuadraticProbing && ./QuadraticProbing
+
+
+
